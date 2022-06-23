8108fdbfbd0e20f85df6e91a28bec9f99702de8c
Angela Hsu <ayh123@gmail.com>
2022-06-23 13:15:36 -0600

    added gh-pages

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <div>
       <small className="footer">
-        Created with <i className="fa fa-solid fa-heart footer-heart-icon"></i> by <i className="fa footer-github-icon">&#xf09b;</i> <a className="footer-link" href="https://github.com/ashoofly" target="_blank" rel="noreferrer">ashoofly</a> for <img className="footer-scrimba-icon" src={scrimbaIcon} alt="scrimba-icon"></img> <a className="footer-link" href="https://scrimba.com/learn/learnreact" target="_blank" rel="noreferrer">Scrimba</a>
+        Created with <i className="fa fa-solid fa-heart footer-heart-icon"></i> by <i className="fa footer-github-icon">&#xf09b;</i> <a className="footer-link" href="https://github.com/ashoofly/scrimba-reactjs-travel-journal" target="_blank" rel="noreferrer">ashoofly</a> for <img className="footer-scrimba-icon" src={scrimbaIcon} alt="scrimba-icon"></img> <a className="footer-link" href="https://scrimba.com/learn/learnreact" target="_blank" rel="noreferrer">Scrimba</a>
       </small>
     </div>
   );
